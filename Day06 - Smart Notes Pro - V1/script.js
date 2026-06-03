9d7b36de9800bdd7f0700371f60d12317594d877
Fahadbinhashim2005 <fahadbinhashim03@gmail.com>
2026-06-03 23:52:46 +0530

    basic js done...

--- Day06 - Smart Notes Pro - V1/script.js
+++ Day06 - Smart Notes Pro - V1/script.js
@@ -1,2 +1,38 @@
-let notes = [];
+let notes = [
+    {
+        id: 1,
+        title: "Project Phoenix",
+        content: "Build Smart Notes App using JavaScript.",
+        category: "Projects"
+    },
+    {
+        id: 2,
+        title: "JavaScript Revision",
+        content: "Learn DOM manipulation and Local Storage.",
+        category: "Study"
+    }
+];
+
+const notesGrid = document.getElementById("notesGrid");
+
+function renderNotes(){
+    notesGrid.innerHTML = "";
+    notes.forEach(note => {
+        const noteCard = document.createElement("div");
+        noteCard.classList.add("note-card");
+        const title = document.createElement("h3");
+        title.textContent = note.title;
+        const content = document.createElement("p");
+        content.textContent = note.content;
+        const category = document.createElement("span");
+        category.textContent = note.category;
+
+        noteCard.appendChild(title);
+        noteCard.appendChild(content);
+        noteCard.appendChild(category);
+        notesGrid.appendChild(noteCard);
+    });
+}
+
+renderNotes();
 
